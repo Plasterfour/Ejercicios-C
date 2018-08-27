2ff21081c7877eb0da1aa6241473fdfc83cfe28c
Plasterfour <42197092+Plasterfour@users.noreply.github.com>
2018-08-26 21:17:14 -0500

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+	int n,i,j,m;
+	scanf("%d", &n);
+	int a = n*2 - 1;
+	for(i=0;i<a;i++){
+		for(j=0;j<a;j++){
+			m = i < j ? i : j;
+			m = m < a-i ? m : a-i-1;
+			m = m < a-j-1 ? m : a-j-1;
+			printf("%d ", n-m);
+		}
+		printf("\n");
+	}
+	return 0;
+}
