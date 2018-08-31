dd3f9a24362099cea6c3e54e4f8fac3d5c2cc513
Plasterfour <42197092+Plasterfour@users.noreply.github.com>
2018-08-30 22:13:26 -0500

    Add files via upload

---
+++ frec_dig.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+	int main() {
+	char n[]="0123456789";
+	char c[1000];
+	int a,b,co;
+	scanf("%s",c);
+	a=strlen(c);
+	b=strlen(n);
+	for(int j=0;j<b;j++)
+	{
+		co=0;
+		for(int i=0;i<a;i++)
+		{
+			if(s[i]==n[j])
+				co++;
+		}
+		printf("%d ",co);
+		
+	}
+	return 0;
+}
---
+++ inv_num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,n,n1,j,x;
+	scanf("%d %d", &n,&n1);
+	int nu[100];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d", &nu[i]);
+	}
+	
+	for (j=0;j>n;j++)
+	{
+	x=nu[j+1];
+	nu[j+1]=nu[0];
+	nu[0]=x;
+	
+		printf("%d ",x);
+	}
+	return 0;
+}
